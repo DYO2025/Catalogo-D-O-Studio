7e8600131775da720b84114fc0d9ed34012ee798
Omelio <dyostudio05142025@gmail.com>
2026-09-23 20:54:28 -0700

    Primera version del catalogo

---
+++ products.js
@@ -0,0 +1,31 @@
+const products = [
+    {
+        id: 1,
+        name: "Chaqueta de cuero",
+        category: "Ropa",
+        priceUSD: 75,
+        priceCUP: 27000,
+        image: "images/chaqueta.jpg",
+        description: "Chaqueta de cuero para hombre."
+    },
+
+    {
+        id: 2,
+        name: "Zapatos deportivos",
+        category: "Zapatos",
+        priceUSD: 60,
+        priceCUP: 21600,
+        image: "images/zapatos.jpg",
+        description: "Zapatos deportivos para uso diario."
+    },
+
+    {
+        id: 3,
+        name: "Smartphone",
+        category: "Electrónica",
+        priceUSD: 250,
+        priceCUP: 90000,
+        image: "images/telefono.jpg",
+        description: "Smartphone disponible por encargo."
+    }
+];
